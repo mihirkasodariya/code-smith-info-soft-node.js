54ebda59d896d665f729f2e9895a139c8ca4e09f
mihirkasodariya <mihirkasodariya21@gmail.com>
2025-06-20 11:43:21 +0530

    - add reame file and add log running server and db connect

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -4,7 +4,7 @@ import { connect } from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await connect(process.env.MONGOURL, {});
-    console.log(`MongoDB Database: ${conn.connection.name} Connected Successfully!\nHost: ${conn.connection.host}\n`);
+    console.log(`→ MongoDB Database: ${conn.connection.name} Connected Successfully!\n→ Host: ${conn.connection.host}\n`);
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
     process.exit(1);
--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.use(express.static(join(__dirname, "public")));
 app.use("/api", router.default);
 
 app.listen(port, () => {
-  console.log("Server is running on port", port);
+  console.log("→ Port : Server is running on port", port);
 });
